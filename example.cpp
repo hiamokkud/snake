687426bf35d662102fe0ab166ba6d0cd87985bfc
hiamokkud <hiamokku@proton.me>
2025-11-30 16:39:09 +0100

    Added snake movement and rendering.

--- example.cpp
+++ example.cpp
@@ -1,15 +1,72 @@
 #include "raylib.h"
-
-int main(void)
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <chrono>
+struct BodyCell
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    int x, y;
+};
+enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right
+};
+int main(int argc, char **argv)
+{
+    int gameFieldWidth = 20, gameFieldHeight = 20;
+    int screenWidth = 800, screenHeight = 600;
+
+    std::vector<BodyCell> snake;
+    snake.push_back(BodyCell{gameFieldWidth / 2, gameFieldHeight / 2});
+    snake.push_back(BodyCell{gameFieldWidth / 2-1, gameFieldHeight / 2});
+    snake.push_back(BodyCell{gameFieldWidth / 2-2, gameFieldHeight / 2});
+
+
+    Direction snakeDirection = Up;
+
+    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - logo raylib");
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        if (IsKeyDown(KEY_A))
+            snakeDirection = Left;
+        else if (IsKeyDown(KEY_S))
+            snakeDirection = Down;
+        else if (IsKeyDown(KEY_D))
+            snakeDirection = Right;
+        else if (IsKeyDown(KEY_W))
+            snakeDirection = Up;
+
+        { // moving snake
+            BodyCell nextPosition = snake[0];
+            std::cout<<nextPosition.x<<" "<<nextPosition.y<<std::endl;
+            if (snakeDirection == Left)
+                nextPosition.x += -1;
+            else if (snakeDirection == Right)
+                nextPosition.x += +1;
+            else if (snakeDirection == Up)
+                nextPosition.y += +1;
+            else if (snakeDirection == Down)
+                nextPosition.y += -1;
+            for (int i = 0; i < snake.size(); i++)
+            {
+                BodyCell currentPosition = snake[i];
+                snake[i] = nextPosition;
+                nextPosition = currentPosition;
+            }
+        }
+
+        ClearBackground(RAYWHITE);
+        for (int i = 0; i < snake.size(); i++)
+        {
+            DrawRectangle(snake[i].x * screenWidth / gameFieldWidth, (gameFieldHeight - snake[i].y - 1) * screenHeight / gameFieldHeight, screenWidth / gameFieldWidth, screenHeight / gameFieldHeight, BLACK);
+        }
+
         EndDrawing();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
     CloseWindow();
